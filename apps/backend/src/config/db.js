ab1fa60a05d5bfe9d750f62526b223ac2150b9aa
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-07-14 23:35:59 -0600

    Creando los modelos de artista, usuarios y canciones

--- apps/backend/src/config/db.js
+++ apps/backend/src/config/db.js
@@ -1,22 +1,25 @@
-import {  Sequelize} from "@sequelize/core";
+import { Sequelize } from "@sequelize/core";
 import { PostgresDialect } from "@sequelize/postgres";
 import config from "./config.js";
-
 export const sequelize = new Sequelize({
-    dialect: PostgresDialect,
-    database: config.dbname,
-    user: config.dbuser,
-    password: config.dbpass,
-    host: 'localhost',
-    port: 5432,
-    ssl: false,
-    clientMinMessages: 'notice',
-})
+	dialect: PostgresDialect,
+	database: config.dbname,
+	user: config.dbuser,
+	password: config.dbpass,
+	host: "localhost",
+	port: 5432,
+	ssl: false,
+    define: {
+        underscored: true
+    },
+	clientMinMessages: "notice",
+});
+
 export const connection = async () => {
 	try {
 		await sequelize.authenticate();
-        console.log('Connection has been established successfully ✅✅✅');
+		console.log("Connection has been established successfully ✅✅✅");
 	} catch (error) {
-        console.log("Error connecting to database ☠️☠️☠️");
-    }
+		console.log("Error connecting to database ☠️☠️☠️");
+	}
 };
---
+++ apps/backend/src/models/index.js
@@ -0,0 +1,126 @@
+import { sequelize } from "../config/db.js";
+import uniqid from "uniqid";
+
+import {
+	Attribute,
+	PrimaryKey,
+	NotNull,
+	Default,
+	Unique,
+	DeletedAt,
+	Index,
+	ColumnName,
+} from "@sequelize/core/decorators-legacy";
+
+const Song = sequelize.define('Song', {
+    id:{
+        type: DataTypes.STRING(18),
+        primaryKey: true, 
+        defaultValue: () => uniqid()
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    album: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    releaseDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+        columnName: 'release_date'
+    },
+    coverImg: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        columnName: 'cover_img'
+    },
+    cloudinaryPublicId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        columnName: 'cloudinary_public_id'
+    },
+    cloudinarySecureUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        columnName: 'cloudinary_secure_url'
+    },
+    artistId: { // TODO: add foreign key
+        type: DataTypes.STRING(18),
+        allowNull: false,   
+    } 
+}, {timestamps: true, tableName: 'songs'})
+
+const Artist = sequelize.define('Artist', {
+    id:{
+        type: DataTypes.STRING(18),
+        primaryKey: true, 
+        defaultValue: () => uniqid()
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    bio: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: 'No bio available'
+    },
+    coverImg: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        columnName: 'cover_img'
+    }
+}, {
+    timestamps: true,
+    tableName: 'artists'
+})
+
+const User = sequelize.define('User', {
+    id:{
+        type: DataTypes.STRING(18),
+        primaryKey: true, 
+        defaultValue: () => uniqid()
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: true,
+    tableName: 'users'
+})
+
+
+Artist.hasMany(Song, {foreignKey:'artistId', as: 'songs'})
+Song.belongsTo(Artist, {foreignKey: "artistId"})
+
+User.hasMany(Artist, {foreignKey: 'userId' })
+Artist.belongsTo(User, {foreignKey: 'user_id'})
+
+sequelize.sync()
+
+export { Song, Artist, User }
