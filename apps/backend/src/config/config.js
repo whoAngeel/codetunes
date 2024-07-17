bcad2c3801cf2f0921dcba701421a7e202adb122
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-07-17 07:29:24 -0600

    configurando las credenciales de cloudinary

--- apps/backend/src/config/config.js
+++ apps/backend/src/config/config.js
@@ -7,6 +7,9 @@ const config = {
 	dbuser: process.env.DB_USER || "",
 	dbpass: process.env.DB_PASS || "",
 	secret: process.env.JWT_KEY,
+	cloud_name : process.env.CLOUD_NAME,
+	cloud_apikey : process.env.CLOUD_APIKEY,
+	cloud_secret: process.env.CLOUD_SECRET
 };
 
 export default config;
