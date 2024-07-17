780f98505855a4000d43c6d8397d8fc2959d9ab3
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-07-17 10:00:57 -0600

    Subiendo archivos de video a cloudinary

--- apps/backend/src/router/songs.routes.js
+++ apps/backend/src/router/songs.routes.js
@@ -1,27 +1,37 @@
 import { Router } from "express";
 import fileUpload from "express-fileupload";
 import { uploadFile } from "../config/cloudinary.js";
-import { uploadSong } from "../services/songs.service.js";
-
+import passport from "passport";
+import { Song } from "../models/index.js";
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const router = Router();
 
-router.use(
+router.use( 
 	fileUpload({
 		useTempFiles: true,
-		tempFileDir: "./src/tmp",
+		tempFileDir: `${__dirname}/tmp`,
 	})
 );
 
-
-router.post("/upload", async (req, res, next) => {
-	try {
-       const song = req.file.song
-       const data = req.body
-       const rta = uploadSong(song, data)
-       res.json((rta))
-	} catch (error) {
-		next(error);
+router.post(
+	"/upload",
+	passport.authenticate("jwt", { session: false }),
+	async (req, res, next) => {
+		try {
+            console.log(__dirname);
+			const result = await uploadFile(req.files.song);
+			const song = await Song.create({
+				...req.body,
+				cloudinaryPublicId: result.public_id,
+				cloudinarySecureUrl: result.secure_url,
+			});
+			res.json(song);
+		} catch (error) {
+			next(error);
+		}
 	}
-});
+);
 
 export default router;
